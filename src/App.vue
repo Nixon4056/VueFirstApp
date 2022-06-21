d2765ba18c876d00b74a2fefd10d415799da6cac
Nikodem <nikodem@nicode.pl>
2022-06-21 20:47:25 +0200

    component friend added

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <h2>Nikodem Wicon</h2>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 'Nikodem',
+          name: 'Nikodem Wicon',
+          phone: '+48 696 787 013',
+          email: 'nikodem@nicode.pl'
+        },
+        {
+          id: 'Marta',
+          name: 'Marta Rybicka',
+          phone: '+48 668 246 135',
+          email: 'nikodem@nesperta.com'
+        }
+      ],
+    };
+  },
+}
+</script>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,32 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone: </strong>{{ friend.phone }}</li>
+      <li><strong>Email: </strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+
+export default{
+  data() {
+    return{
+      detailsAreVisible: false,
+      friend: {
+        id: 'Nikodem',
+        name: 'Nikodem Wicon',
+        phone: '+48 696 787 013',
+        email: 'nikodem@nicode.pl'
+      },
+    };
+  },
+  methods: {
+    toggleDetails(){
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
